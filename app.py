6949c1953ed1cee40cb25f040843adde34684143
Iago Rozales Mahiques <iagomahiques@gmail.com>
2026-03-11 09:06:48 -0300

    Incluido o reclamante no app.py

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from item.src.presentation.api.routes import item_routes
 from responsavel.src.presentation.api.routes import responsavel_routes
 from local.src.presentation.api.routes import local_routes
 from devolucao.src.presentation.api.routes import devolucao_routes
+from reclamante.src.presentation.api.routes import reclamante_routes
 # banco de dados
 from local.src.infrastructure.database.config import init_db as init_db_local
 from item.src.infrastructure.database.config import init_db as init_db_item
 from responsavel.src.infrastructure.database.config import init_db as init_db_responsavel
 from devolucao.src.infrastructure.database.config import init_db as init_db_devolucao
+from reclamante.src.infrastructure.database.config import init_db as init_db_reclamante
 
 
 @asynccontextmanager
@@ -20,6 +22,7 @@ async def lifespan(app: FastAPI):
     await init_db_responsavel()
     await init_db_local()
     await init_db_devolucao()
+    await init_db_responsavel()
     yield
 
 app = FastAPI(
@@ -34,6 +37,7 @@ app.include_router(item_routes.router, prefix="/api/v1/items")
 app.include_router(responsavel_routes.router, prefix="/api/v1/responsaveis")
 app.include_router(local_routes.router, prefix="/api/v1/local")
 app.include_router(devolucao_routes.router, prefix="/api/v1/devolucoes")
+app.include_router(reclamante_routes.router, prefix="/api/v1/reclamantes")
 
 @app.get("/")
 def read_root():
