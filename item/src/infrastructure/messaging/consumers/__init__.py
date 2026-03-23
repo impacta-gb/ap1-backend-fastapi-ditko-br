adaa08d115ab9aef89279e6b5bd7f407613fd00a
Felipe Viana <felipe.wilson@aluno.faculdadeimpacta.com.br>
2026-03-23 20:36:37 -0300

    feat: incluindo consumers de local e responsavel

--- item/src/infrastructure/messaging/consumers/__init__.py
+++ item/src/infrastructure/messaging/consumers/__init__.py
@@ -2,5 +2,7 @@
 Módulo de consumers para o microserviço de Item
 """
 from item.src.infrastructure.messaging.consumers.devolucao_event_consumer import DevolucaoEventConsumer
+from item.src.infrastructure.messaging.consumers.local_event_consumer import LocalEventConsumer
+from item.src.infrastructure.messaging.consumers.responsavel_event_consumer import ResponsavelEventConsumer
 
-__all__ = ['DevolucaoEventConsumer']
+__all__ = ['DevolucaoEventConsumer', 'LocalEventConsumer', 'ResponsavelEventConsumer']
